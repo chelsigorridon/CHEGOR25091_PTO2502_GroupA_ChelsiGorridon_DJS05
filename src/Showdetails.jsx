cb7832dba2df36d5d4cab26f93198a87516782f6
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-11-07 19:07:53 +0200

    Added Imports to Show Details

--- src/Showdetails.jsx
+++ src/Showdetails.jsx
@@ -1,1 +1,17 @@
-export const Showdetails = {};
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { PodcastProvider } from "./context/PodcastContext";
+import { fetchPodcasts } from "./api/fetchPodcasts";
+import { genres } from "./data";
+
+
+export default function ShowDetails() {
+  const { podcastId } = useParams();
+
+  return (
+    <div>
+      <h1>Podcast Details</h1>
+      <p>Podcast ID: {podcastId}</p>
+    </div>
+  );
+}
