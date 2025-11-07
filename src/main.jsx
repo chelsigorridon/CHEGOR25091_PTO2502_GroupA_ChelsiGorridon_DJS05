089a7ae0aec253caf0d7d54447dc7dcfba365173
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-11-07 11:32:58 +0200

    Added CreatebrowserRouter to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+
+const Router = createBrowserRouter([]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <RouterProvider router={Router} />
   </StrictMode>,
 )
